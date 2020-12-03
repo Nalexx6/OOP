9cf180a60e81d052391811d881b36535f19f1e34
Nalexx6 <nikita2002oleksienko@gmail.com>
2020-12-03 23:38:16 +0200

    Assign detected images to points(objects)

--- 1 semester/Opencv_python/Template_matching/main.py
+++ 1 semester/Opencv_python/Template_matching/main.py
@@ -1,49 +1,76 @@
 import cv2 as cv
 import numpy as np
 
-haystack_img = cv.imread("albion_farm.jpg", cv.IMREAD_UNCHANGED)
-needle_img = cv.imread("albion_cabbage.jpg", cv.IMREAD_UNCHANGED)
 
-needle_w = needle_img.shape[1]
-needle_h = needle_img.shape[0]
+def findClickPositions(needle_img_path, haystack_img_path, threshold=0.5, debug_mode=None):
 
-result = cv.matchTemplate(haystack_img, needle_img, cv.TM_SQDIFF_NORMED)
+    haystack_img = cv.imread(haystack_img_path, cv.IMREAD_UNCHANGED)
+    needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
+    # Save the dimensions of the needle image
+    needle_w = needle_img.shape[1]
+    needle_h = needle_img.shape[0]
 
-threshold = 0.17
-locations = np.where(result <= threshold)
-locations = list(zip(*locations[::-1]))
+    # There are 6 methods to choose from:
+    # TM_CCOEFF, TM_CCOEFF_NORMED, TM_CCORR, TM_CCORR_NORMED, TM_SQDIFF, TM_SQDIFF_NORMED
+    method = cv.TM_CCOEFF_NORMED
+    result = cv.matchTemplate(haystack_img, needle_img, method)
 
-# creating the list of rectangles(x, y, w, h)
-rectangles = []
-for loc in locations:
-    rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
-    rectangles.append(rect)
-    rectangles.append(rect) # need to do it twice because cv.groupRectangles make rectangle to disappear if
-                            # there are no other overlapping rectangles
+    locations = np.where(result >= threshold)
+    locations = list(zip(*locations[::-1]))
+    # print(locations)
 
 
+    rectangles = []
+    for loc in locations:
+        rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
+        # Add every box to the list twice in order to retain single (non-overlapping) boxes
+        rectangles.append(rect)
+        rectangles.append(rect)
 
-rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
-print(rectangles)
+    rectangles, weights = cv.groupRectangles(rectangles, groupThreshold=1, eps=0.5)
+    # print(rectangles)
+
+    points = []
+    if len(rectangles):
+        # print('Found needle.')
+
+        line_color = (0, 255, 0)
+        line_type = cv.LINE_4
+        marker_color = (255, 0, 255)
+        marker_type = cv.MARKER_CROSS
+
+        # Loop over all the rectangles
+        for (x, y, w, h) in rectangles:
+
+            # Determine the center position
+            center_x = x + int(w / 2)
+            center_y = y + int(h / 2)
+            # Save the points
+            points.append((center_x, center_y))
+
+            if debug_mode == 'rectangles':
+                # Determine the box position
+                top_left = (x, y)
+                bottom_right = (x + w, y + h)
+                # Draw the box
+                cv.rectangle(haystack_img, top_left, bottom_right, color=line_color,
+                             lineType=line_type, thickness=2)
+            elif debug_mode == 'points':
+                # Draw the center point
+                cv.drawMarker(haystack_img, (center_x, center_y),
+                              color=marker_color, markerType=marker_type,
+                              markerSize=40, thickness=2)
+
+        if debug_mode:
+            cv.imshow('Matches', haystack_img)
+            cv.waitKey()
+
+    return points
 
 
-if len(rectangles):
-    print("Found needle")
-
-    line_color = (0, 255, 0)
-    line_type = cv.LINE_4
-
-    # need to loop over the locations and draw their rectangle
-    for (x, y, w, h) in rectangles:
-
-        top_left = (x, y)
-        bottom_right = (x + w, y + h)
-
-        cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
-
-
-    cv.imshow("Matches", haystack_img)
-    cv.waitKey()
-
-else:
-    print("Needle not found")
+points = findClickPositions('albion_cabbage.jpg', 'albion_farm.jpg', debug_mode='points')
+print(points)
+points = findClickPositions('albion_turnip.jpg', 'albion_farm.jpg',
+                            threshold=0.70, debug_mode='rectangles')
+print(points)
+print('Done.')
