a85c2878d7c961d48fbe08e0c189996eeee4847e
Nalexx6 <nikita2002oleksienko@gmail.com>
2020-12-03 22:44:11 +0200

    Thresholding to match more templates(and playing with comparison method)

--- 1 semester/Opencv_python/Template_matching/main.py
+++ 1 semester/Opencv_python/Template_matching/main.py
@@ -1,34 +1,34 @@
 import cv2 as cv
 import numpy as np
 
-haystack_img = cv.imread("unnamed.jpg", cv.IMREAD_REDUCED_COLOR_2)
-needle_img = cv.imread("De Bruyne.png", cv.IMREAD_REDUCED_COLOR_2)
+haystack_img = cv.imread("albion_farm.jpg", cv.IMREAD_UNCHANGED)
+needle_img = cv.imread("albion_cabbage.jpg", cv.IMREAD_UNCHANGED)
 
-result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCORR_NORMED)
+result = cv.matchTemplate(haystack_img, needle_img, cv.TM_SQDIFF_NORMED)
 
+threshold = 0.17
+locations = np.where(result <= threshold)
+locations = list(zip(*locations[::-1]))
+print(locations)
 
-#get the best match position
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+if locations:
+    print("Found needle")
 
-print('Best match top left postion: %s' % str(max_loc))
-print('Best match confidence: %s' % max_val)
-
-threshold = 0.5
-
-if(max_val >= threshold):
-    print("Found neeedle.")
-
-    #get dimension of the needle image
     needle_w = needle_img.shape[1]
     needle_h = needle_img.shape[0]
+    line_color = (0, 255, 0)
+    line_type = cv.LINE_4
 
-    top_left = max_loc
-    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+    #need to loop over the locations and draw their rectangle
+    for loc in locations:
 
-    cv.rectangle(haystack_img, top_left, bottom_right,
-                 color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
+        top_left = loc
+        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
 
-    cv.imshow("Result", haystack_img)
+        cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
+
+
+    cv.imshow("Matches", haystack_img)
     cv.waitKey()
 
 else:
