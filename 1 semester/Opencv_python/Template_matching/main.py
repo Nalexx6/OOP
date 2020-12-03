ef75cc7a0d697673e29bab774eb97cb5d1801cfd
Nalexx6 <nikita2002oleksienko@gmail.com>
2020-12-03 23:32:32 +0200

    Group overlapping rectangles

--- 1 semester/Opencv_python/Template_matching/main.py
+++ 1 semester/Opencv_python/Template_matching/main.py
@@ -4,26 +4,40 @@ import numpy as np
 haystack_img = cv.imread("albion_farm.jpg", cv.IMREAD_UNCHANGED)
 needle_img = cv.imread("albion_cabbage.jpg", cv.IMREAD_UNCHANGED)
 
+needle_w = needle_img.shape[1]
+needle_h = needle_img.shape[0]
+
 result = cv.matchTemplate(haystack_img, needle_img, cv.TM_SQDIFF_NORMED)
 
 threshold = 0.17
 locations = np.where(result <= threshold)
 locations = list(zip(*locations[::-1]))
-print(locations)
 
-if locations:
+# creating the list of rectangles(x, y, w, h)
+rectangles = []
+for loc in locations:
+    rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
+    rectangles.append(rect)
+    rectangles.append(rect) # need to do it twice because cv.groupRectangles make rectangle to disappear if
+                            # there are no other overlapping rectangles
+
+
+
+rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
+print(rectangles)
+
+
+if len(rectangles):
     print("Found needle")
 
-    needle_w = needle_img.shape[1]
-    needle_h = needle_img.shape[0]
     line_color = (0, 255, 0)
     line_type = cv.LINE_4
 
-    #need to loop over the locations and draw their rectangle
-    for loc in locations:
+    # need to loop over the locations and draw their rectangle
+    for (x, y, w, h) in rectangles:
 
-        top_left = loc
-        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+        top_left = (x, y)
+        bottom_right = (x + w, y + h)
 
         cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
 
