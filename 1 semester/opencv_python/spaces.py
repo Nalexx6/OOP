d9c85ded09516d0f2a8e2f6e373007d1a7286b4c
N_alexx <nikita2002oleksienko@gmail.com>
2020-12-07 13:23:55 +0200

    Color spaces and conversion

---
+++ 1 semester/opencv_python/spaces.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('Photos/park.jpg')
+cv.imshow('Park', img)
+
+# BGR -> Grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# BGR -> HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow('HSV', hsv)
+
+# BGR -> L*a*b
+lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+cv.imshow('LAB', lab)
+
+# BGR -> RGB, we see colors inversion because OpenCV displays images in BGR format
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow('RGB', rgb)
+
+
+cv.waitKey(0)
