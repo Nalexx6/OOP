b1ebfdc7fdd333d181e7575888353b6b3f9f5566
N_alexx <nikita2002oleksienko@gmail.com>
2021-02-20 17:09:30 +0200

    OpenCV project final cascade(unfortunately, worse than previous)

--- 1 semester/Object_detection_project/main.py
+++ 1 semester/Object_detection_project/main.py
@@ -3,9 +3,9 @@
 # import cv2 as cv
 # from time import time
 # from cascade_utils import generate_negative_description_file
-
+#
 # generate_negative_description_file()
-
+#
 # points = tm.findClickPositions('albion_cabbage.jpg', 'albion_farm.jpg', debug_mode='points')
 # print(points)
 # points = tm.findClickPositions('albion_turnip.jpg', 'albion_farm.jpg',
@@ -13,73 +13,46 @@
 # print(points)
 # print('Done.')
 
-# wincap = WindowCapture("Chess")
-
-# loop_time = time()
-# while(True):
-
-#     # get an updated image of the game
-#     screenshot = wincap.get_screenshot()
-
-#     cv.imshow("Computer Vision", screenshot)
-
-#     # debug the loop rate
-#     print('FPS {}'.format(1 / (time() - loop_time)))
-#     loop_time = time()
-
-#     # press ESC to exit.
-#     if cv.waitKey(1) == 27:
-#         cv.destroyAllWindows()
-#         break
-
-# print("Done")
-
-from __future__ import print_function
 import cv2 as cv
-import argparse
-def detectAndDisplay(frame):
-    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    frame_gray = cv.equalizeHist(frame_gray)
-    #-- Detect faces
-    faces = face_cascade.detectMultiScale(frame_gray)
-    for (x,y,w,h) in faces:
-        center = (x + w//2, y + h//2)
-        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
-        faceROI = frame_gray[y:y+h,x:x+w]
-        #-- In each face, detect eyes
-        eyes = eyes_cascade.detectMultiScale(faceROI)
-        for (x2,y2,w2,h2) in eyes:
-            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-            radius = int(round((w2 + h2)*0.25))
-            frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
-    cv.imshow('Capture - Face detection', frame)
-parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-parser.add_argument('--face_cascade', help='Path to face cascade.', default='cascade/cascade.xml')
-parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='D:/opencv-3.4/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
-parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
-args = parser.parse_args()
-face_cascade_name = args.face_cascade
-eyes_cascade_name = args.eyes_cascade
-face_cascade = cv.CascadeClassifier()
-eyes_cascade = cv.CascadeClassifier()
-#-- 1. Load the cascades
-if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
-    print('--(!)Error loading face cascade')
-    exit(0)
-if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
-    print('--(!)Error loading eyes cascade')
-    exit(0)
-camera_device = args.camera
-#-- 2. Read the video stream
-cap = cv.VideoCapture(camera_device)
-if not cap.isOpened:
-    print('--(!)Error opening video capture')
-    exit(0)
-while True:
-    ret, frame = cap.read()
-    if frame is None:
-        print('--(!) No captured frame -- Break!')
+import numpy as np
+from time import time
+from windowcapture import WindowCapture
+from vision import Vision
+from PIL import ImageGrab
+
+
+
+# initialize the WindowCapture class
+wincap = WindowCapture('Task Manager')
+# initialize the Vision class
+# vision_limestone = Vision('albion_limestone.jpg')
+
+
+loop_time = time()
+while(True):
+
+    # get an updated image of the game
+
+    screenshot = wincap.get_screenshot()
+    # screenshot = ImageGrab.grab()
+    # screenshot = np.array(screenshot)
+    # screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+
+    cv.imshow('Computer Vision', screenshot)
+
+    # display the processed image
+    # points = vision_limestone.find(screenshot, 0.5, 'rectangles')
+    #points = vision_gunsnbottle.find(screenshot, 0.7, 'points')
+
+    # debug the loop rate
+    print('FPS {}'.format(1 / (time() - loop_time)))
+    loop_time = time()
+
+    # press 'q' with the output window focused to exit.
+    # waits 1 ms every loop to process key presses
+    if cv.waitKey(1) == 27:
+        cv.destroyAllWindows()
         break
-    detectAndDisplay(frame)
-    if cv.waitKey(10) == 27:
-        break
+
+print('Done.')
+
